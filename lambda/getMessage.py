37c09c90cb0537b0b5426f6ec67242efa4f9c606
Geoff Baskwill <me@geoffbaskwill.ca>
2017-10-14 16:37:43 -0400

    Fix json serialization

--- lambda/getMessage.py
+++ lambda/getMessage.py
@@ -3,6 +3,20 @@ from boto3.dynamodb.conditions import Key, Attr
 import boto3
 import json
 import os
+from decimal import Decimal
+
+class fakefloat(float):
+    def __init__(self, value):
+        self._value = value
+    def __repr__(self):
+        return str(self._value)
+
+def defaultencode(o):
+    if isinstance(o, Decimal):
+        # Subclass float with custom repr?
+        return fakefloat(o)
+    raise TypeError(repr(o) + " is not JSON serializable")
+
 
 print('Loading function')
 
@@ -13,7 +27,7 @@ table = dynamodb.Table(os.environ['TABLE'])
 def respond(code, res=None):
     return {
         'statusCode': code if code else '200',
-        'body': None if code else json.dumps(res),
+        'body': None if code else json.dumps(res, default=defaultencode),
         'headers': {
             'Content-Type': 'application/json',
         },
@@ -29,7 +43,7 @@ def lambda_handler(event, context):
 
     items = response['Items']
     if len(items) > 0:
-        messages = items[0].get('messages', []))
+        messages = items[0].get('messages', [])
 
         for message in messages:
             if message['id'] == event['pathParameters']['messageID']:
