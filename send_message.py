1f7d9f062946862bfa2caa11f5ff880bc2c56656
Rachel Hartviksen <rhartviksen@industrialagency.ca>
2017-10-14 16:13:05 -0400

    Add call to add upcoming schedules to people from the database and then check those people for messages. Note: the method does not post back to update the people's lastMessageSent

--- send_message.py
+++ send_message.py
@@ -1,7 +1,14 @@
 from twilio.rest import Client
+from boto3.dynamodb.conditions import Key, Attr
+import boto3
 import datetime
+import os
 import time
+import uuid
 
+dynamodb = boto3.resource('dynamodb')
+
+# Assumptions about the schedule of the (lamba) scheduler that periodically runs the code
 script_interval_minutes = 60
 error_buffer_minutes = 2
 
@@ -238,3 +245,55 @@ evan = {
 
 upcomingMessages(evan)
 upcomingMessages(adrian)
+
+
+def schedule(datetime):
+  return {
+    'id': str(uuid.uuid4()),
+    'year': datetime.year,
+    'month': datetime.month,
+    'dayOfMonth': datetime.day,
+    'hour': datetime.hour,
+    'minute': datetime.minute
+  }
+
+def scheduleSomeMessagesSoon(person):
+
+  table = dynamodb.Table(os.environ.get('DATABASE_NAME'))
+
+  if not 'schedules' in person:
+      person['schedules'] = []
+
+  now = datetime.datetime.now()
+  very_soon = now + datetime.timedelta(0, 10)
+  soon = now + datetime.timedelta(0,60 * 10)
+  in_an_hour = now + datetime.timedelta(0, 60 * 60)
+  person['schedules'] += [schedule(very_soon)]
+  person['schedules'] += [schedule(soon)]
+  person['schedules'] += [schedule(in_an_hour)]
+
+  table.update_item(
+    Key={
+        'id': person['id']
+    },
+    UpdateExpression='SET schedules = :schedules',
+    ExpressionAttributeValues={
+        ':schedules': person['schedules']
+    }
+  )
+
+def getAllPeople():
+  table = dynamodb.Table(os.environ.get('DATABASE_NAME'))
+  response = table.scan()
+  return response['Items']
+
+
+def checkAllPeopleForMessages():
+  people = getAllPeople()
+
+  for index in range(len(people)):
+    scheduleSomeMessagesSoon(people[index])
+    time.sleep(10)
+    upcomingMessages(people[index])
+
+checkAllPeopleForMessages()
