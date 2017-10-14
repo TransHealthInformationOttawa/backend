f225b12b5aa76f484de8fe4720476bf25077afed
Geoff Baskwill <me@geoffbaskwill.ca>
2017-10-14 14:16:32 -0400

    Add GET support for people, schedules, and messages
    - Created lambdas to read people, schedules, and messages
      out of the database.

---
+++ lambda/listPeople.py
@@ -0,0 +1,26 @@
+import boto3
+import json
+
+print('Loading function')
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE'])
+
+
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    # print("Received event: " + json.dumps(event, indent=2))
+
+    response = table.scan()
+    items = response['Items']
+
+    return respond(None, items)
---
+++ lambda/listSchedules.py
@@ -0,0 +1,33 @@
+from boto3.dynamodb.conditions import Key, Attr
+
+import boto3
+import json
+
+print('Loading function')
+
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table(os.environ['TABLE'])
+
+
+def respond(code, res=None):
+    return {
+        'statusCode': code if code else '200',
+        'body': None if code else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+
+def lambda_handler(event, context):
+    # print("Received event from apigw: " + json.dumps(event, indent=2))
+
+    response = table.query(
+        KeyConditionExpression=Key('id').eq(event['pathParameters']['id'])
+    )
+
+    items = response['Items']
+    if len(items) > 0:
+        return respond(None, items[0].get('schedules', []))
+    else:
+        return respond(404, None)
