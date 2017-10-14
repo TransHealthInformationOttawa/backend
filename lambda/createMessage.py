9700deb8ae7778838e7ac9b55d49752cd08c71e9
Geoff Baskwill <me@geoffbaskwill.ca>
2017-10-14 19:38:20 -0400

    Fix header checking

--- lambda/createMessage.py
+++ lambda/createMessage.py
@@ -38,7 +38,7 @@ def respond(code, res=None):
 def lambda_handler(event, context):
     # print("Received event from apigw: " + json.dumps(event, indent=2))
 
-    if event['headers'].get('Content-Type', None) != 'application/json':
+    if event['headers'].get('content-type', None) != 'application/json' and event['headers'].get('Content-Type', None) != 'application/json':
         return respond(415, {})
 
     body = json.loads(event['body'])
